4b1e022235e9093a420f6837ade1f2542b5152f3
priyac0602 <kusumapriyachowdary011@gmail.com>
2025-06-09 17:11:36 +0530

    Add files via upload

--- review.py
+++ review.py
@@ -7,7 +7,7 @@ conn=snowflake.connector.connect(
     password=st.secrets["snowflake"]["password"],
     account=st.secrets["snowflake"]["account"],
     warehouse=st.secrets["snowflake"]["warehouse"],
-    database=st.secrets["snowflake"]["database"]
+    database=st.secrets["snowflake"]["database"],
     schema=st.secrets["snowflake"]["schema"]
 )
 
